a9cefd9b918bab58bcefa4c64d0864ec4ac56b46
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-06-27 19:05:59 +0800

    add: test for fibonacci 15

--- Fibonacci/fibonacci.test.js
+++ Fibonacci/fibonacci.test.js
@@ -4,6 +4,10 @@ describe("fibonacci", () => {
   test("Regular function ", () => {
     expect(fibs(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
   });
+  test("fibonacci of 15", () => {
+    expect(fibs(15).at(-1)).toEqual(377);
+  });
+
   test.skip("Using a recursive function", () => {
     expect(fibsRec(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
   });
