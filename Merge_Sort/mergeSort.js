03ed3a29bada376c84158de5551919f1c9d75262
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-07-01 19:57:28 +0800

    passed: merge sort test

--- Merge_Sort/mergeSort.js
+++ Merge_Sort/mergeSort.js
@@ -1,6 +1,22 @@
 const mergeSort = function (arr) {
-  if (arr.length <= 1) return arr;
-};
+  if (arr.length < 2) return arr;
+  const result = [];
 
-console.log(mergeSort([]));
+  const half = Math.ceil(arr.length / 2);
+  const left = mergeSort(arr.slice(0, half));
+  const right = mergeSort(arr.slice(half));
+
+  let i = 0,
+    j = 0;
+  while (i < left.length && j < right.length) {
+    console.log("ruuned");
+    if (left[i] < right[j]) {
+      result.push(left[i++]);
+    } else {
+      result.push(right[j++]);
+    }
+  }
+
+  return result.concat(left.slice(i)).concat(right.slice(j));
+};
 export { mergeSort };
