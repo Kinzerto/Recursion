a8d867696196a7cfca7c146763d9ec29aae68705
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-07-01 20:00:02 +0800

    test: merge sort test

--- Merge_Sort/mergeSort.test.js
+++ Merge_Sort/mergeSort.test.js
@@ -7,4 +7,17 @@ describe("Merge sort", () => {
   test("Return single value", () => {
     expect(mergeSort([73])).toEqual([73]);
   });
+  test("return already arranged values", () => {
+    expect(mergeSort([1, 2, 3, 4, 5])).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  test("return sorted val ", () => {
+    expect(mergeSort([3, 2, 1, 13, 8, 5, 0, 1])).toEqual([
+      0, 1, 1, 2, 3, 5, 8, 13,
+    ]);
+  });
+
+  test("Big Numbers sorted ", () => {
+    expect(mergeSort([105, 79, 100, 110])).toEqual([79, 100, 105, 110]);
+  });
 });
