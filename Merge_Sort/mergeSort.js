d5f3a3fa87fc74a97129df14d7d76fc5170a65d4
Kinth Enrique Cabuello <blood4249@gmail.com>
2026-07-02 01:09:38 +0800

    refactor:  change return to spread operator for more readability and left a comment for coders

--- Merge_Sort/mergeSort.js
+++ Merge_Sort/mergeSort.js
@@ -2,6 +2,7 @@ const mergeSort = function (arr) {
   if (arr.length < 2) return arr;
   const result = [];
 
+  //sliced the array
   const half = Math.ceil(arr.length / 2);
   const left = mergeSort(arr.slice(0, half));
   const right = mergeSort(arr.slice(half));
@@ -9,6 +10,7 @@ const mergeSort = function (arr) {
   let i = 0;
   let j = 0;
 
+  // compare left and right and push the small value up to  large value
   while (i < left.length && j < right.length) {
     if (left[i] < right[j]) {
       result.push(left[i++]);
@@ -16,7 +18,7 @@ const mergeSort = function (arr) {
       result.push(right[j++]);
     }
   }
-  //return result and grab leftover value
-  return result.concat(left.slice(i)).concat(right.slice(j));
+  //return result and grab leftover value in either left or right
+  return [...result, ...left.slice(i), ...right.slice(j)];
 };
 export { mergeSort };
